ddd1b908cc8739c5cf6e51cbf040e81109d795a8
GiaNik0la <58341157+GiaNik0la@users.noreply.github.com>
2021-02-20 04:10:25 +0800

    Update main.py
    changing NAAAAAAAAAAAAAAMESSSSSSSSSSSS

--- main.py
+++ main.py
@@ -2,24 +2,24 @@
 import random
 
 #Creating function
-def estiminate_pi(n):
+def calculate_pi(n):
   #Creating variables for count points -_-
-  num_point_circle = 0
-  num_point_total = 0
+  point_in_circle = 0
+  point_in_square = 0
   
   #LOOOOOOOOOOOOOOOOOOOOOOOOOOOOP
-  for J in range(n):
+  for Jemala in range(n):
     #Creating variables for give point an x and y options
     x = random.uniform(0, 1)
     y = random.uniform(0, 1)
     #That's basicly sum of x and y squares
-    distance = x**2 + y**2
+    social_distance_D = x**2 + y**2
     #Checking if point is inside compile
     if distance <= 1:
       #Counting points in circle
-      num_point_circle+=1
+      point_in_circle+=1
     #Counting all points
-    num_point_total+=1
+    point_in_square+=1
   
   #CALCULATIOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOON
-  return 4 * num_point_circle/num_point_total
+  return 4 * point_in_circle/point_in_square
